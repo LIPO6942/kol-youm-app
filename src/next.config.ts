377a4860aa68bc2e6634e1aed1abd6a408401387
Lipo <lipo6942@gmail.com>
2025-08-14 17:57:46 +0000

    Que j ouvre l app, l'icone de l'application ne correspond pas  au backgr

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,4 +1,19 @@
 import type {NextConfig} from 'next';
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -26,4 +41,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
